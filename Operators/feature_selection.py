9e74ac80ee3e68966df6cab0a680103a9aff1fa3
mionaD-upc <miona.dimic@estudiantat.upc.edu>
2023-01-13 15:24:27 +0100

    modified operators

--- Operators/feature_selection.py
+++ Operators/feature_selection.py
@@ -1,42 +1,51 @@
-from sklearn.model_selection import train_test_split
-import sys
-
+import os, sys
 import utils 
-import numpy as np 
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
-# Transformation
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import power_transform
 from sklearn.pipeline import Pipeline
-# Feature Selection
 import sklearn_relief as sr
-# Models
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-
+import warnings
+warnings.filterwarnings('ignore')
 
 def feature_selection(path):
-    data = utils.DBtable_to_df(f'{path}/integration.duckdb', 'sandbox')
+    data = utils.DBtable_to_df('../integration.duckdb', 'integratedTable')
     dataN = data.drop(columns=['Year','Madrid_section'])
     dataN = dataN.astype(float)
 
+    l = len(dataN.columns)
+    cols  =  dataN.columns.to_list()
+
     dataN.reset_index(drop=True, inplace=True)
+
     scaler = MinMaxScaler()
+
     dataN = power_transform(dataN, method='yeo-johnson')
     dataN = scaler.fit_transform(dataN)
 
-    X = dataN[:,0:22]
-    y = dataN[:,22]
+    X = dataN[:,0:l-1]
+    y = dataN[:,l-1]
 
+    np.random.seed(144)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state = 0)
+    r = sr.RReliefF(n_features = 5)
+    r.fit_transform(X_train,y_train)
+    values = r.w_
+    res = dict(zip(cols, values))
 
-    nof_list=np.arange(1,5)     
-    print(nof_list)       
+    #print(sorted(res.items(), key=lambda x:x[1],reverse=True))
+    print("Top five important features based on RReliefF Filter Method")
+    print(dict(sorted(res.items(), key=lambda x:x[1],reverse=True)[0:5]).keys())
+
+    np.random.seed(144)
+    nof_list=np.arange(1,11)     
     high_score=0
     nof=0           
     score_list =[]
+    
     for n in range(len(nof_list)):
         X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state = 0)
         fs = sr.RReliefF(n_features = nof_list[n])
@@ -44,13 +53,11 @@ def feature_selection(path):
         relief.fit(X_train,y_train)
         score = relief.score(X_test,y_test)
         score_list.append(score)
-        print(f'NOF: {nof_list[n]}, Score: {score}')
         if(score > high_score):
             high_score = score
             nof = nof_list[n]
-
-    print (print(f'High Score: NOF: {nof}, Score: {high_score}'))
-
+        print('Highest Score for Number of Features used in RReliefF Wrapper Method:')
+        print(high_score)
 
 def main():
     print('\nSTART FEATURE SELECTION')
